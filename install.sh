cd124c7b9db4800cdd91ad7073aa3a001e320af9
Michael Nelson <michaelmannelson@users.noreply.github.com>
2021-03-12 09:38:10 -0600

    Add files via upload

--- install.sh
+++ install.sh
@@ -52,11 +52,10 @@ if [ $argHelp -eq 1 ]; then
     exit 0
 fi
 
-#sudo apt-get update && apt-get upgrade -y
-#sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y     # https://xmrig.com/docs/miner/build/ubuntu
-#sudo apt-get install wget proot libmicrohttpd-dev -y                                     # https://github.com/cmxhost/xmrig/blob/master/README.md
-##apt-get install wget proot libmicrohttpd-dev openssl -y                            # https://github.com/cmxhost/xmrig/blob/master/README.md
-#sudo apt-get install jq -y
+sudo apt-get update && apt-get upgrade -y
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y     # https://xmrig.com/docs/miner/build/ubuntu
+sudo apt-get install wget proot libmicrohttpd-dev -y                                     # https://github.com/cmxhost/xmrig/blob/master/README.md
+sudo apt-get install jq -y
 
 while [ ! -f "$argConfig" ]; do read -p "config: " argConfig; done
 if [ "$argUrl" == "" ]; then argUrl=`jq ".pools[].url" "$argConfig" | sed -e 's/^"//' -e 's/"$//'`; fi
