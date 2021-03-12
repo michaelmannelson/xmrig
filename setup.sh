1ed133c0ae81c974c301bbbb6c4d96790617752e
Michael Nelson <michaelmannelson@users.noreply.github.com>
2021-03-12 07:33:33 -0600

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -12,6 +12,7 @@ mv -f xmrig-main/* "$HOME/xmrig"
 rm -rf xmrig-main/
 rm -f main.zip
 
+echo
 read -p "url: " url
 read -p "user: " user
 echo "$HOME/xmrig/install.sh -o \"$url\" -u \"$user\" -p \"`uname -o`.`uname -s`.`uname -n`.`uname -m`.$(date +%Y%m%d@%H%M%S%z)\" -c \"$HOME/xmrig/config.json\""
